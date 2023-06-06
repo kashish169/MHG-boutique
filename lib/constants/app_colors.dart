f002f1608ef411dfca5f238e7832ed258b1ca745
AliNezamli <alinezamli54@gmail.com>
2023-06-06 14:35:51 +0300

    updates

--- lib/constants/app_colors.dart
+++ lib/constants/app_colors.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static Color primary = Colors.black;
+  static Color primary = const Color(0xFF232323);
   static Color secondary = Colors.black;
+  static Color label = const Color(0xFF333333);
+  static Color lightLabel = const Color(0xFF9B9B9B);
   static Color grey = Colors.grey;
   static Color white = Colors.white;
-  static Color label = Colors.black;
 }
