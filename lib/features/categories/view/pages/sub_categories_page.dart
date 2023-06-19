b6325a2999552f0fe76fb12a6e2a353548478c4a
AliNezamli <alinezamli54@gmail.com>
2023-06-19 10:27:04 +0300

    fixes

--- lib/features/categories/view/pages/sub_categories_page.dart
+++ lib/features/categories/view/pages/sub_categories_page.dart
@@ -1,13 +1,8 @@
 import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mhg/features/categories/controller/categories_controller.dart';
 import 'package:mhg/features/categories/models/categories_model.dart';
 import 'package:mhg/widgets/custom_app_bar.dart';
-import 'package:mhg/widgets/loading_widget.dart';
-import 'package:mhg/widgets/retry_button.dart';
-import '../widgets/categoies_explore_our_brands_card.dart';
 import '../widgets/category_list_tile.dart';
 
 class SubCategoriesPage extends StatelessWidget {
@@ -17,21 +12,19 @@ class SubCategoriesPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final List  data=ModalRoute.of(context)!.settings.arguments as List;
-    List<Menu> categoriesModel=data![0] ;
-    String title=data![1] ;
+    final List data = ModalRoute.of(context)!.settings.arguments as List;
+    List<Menu> categoriesModel = data![0];
+    String title = data![1];
     log(Get.arguments.toString());
-    return   Scaffold(
-      appBar: customAppBar(context,title: title),
+    return Scaffold(
+      appBar: customAppBar(context, title: title),
       body: SafeArea(
         child: SingleChildScrollView(
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            children:  [
-              for(int i=0 ;i<categoriesModel.length;i++)
+            children: [
+              for (int i = 0; i < categoriesModel.length; i++)
                 CategoryListTile(model: categoriesModel[i]),
-
-
               SizedBox(
                 height: 80,
               )
@@ -40,9 +33,5 @@ class SubCategoriesPage extends StatelessWidget {
         ),
       ),
     );
-
-
-
-
   }
 }
