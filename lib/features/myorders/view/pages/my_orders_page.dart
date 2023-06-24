67d426a807f8ac9f6afcf623fdc5131fad29185f
AliMzek <ali_mzek@outlook.com>
2023-06-24 12:16:44 +0300

    ordersListApi

--- lib/features/myorders/view/pages/my_orders_page.dart
+++ lib/features/myorders/view/pages/my_orders_page.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:mhg/constants/app_colors.dart';
+import 'package:mhg/features/myorders/controller/my_orders_controller.dart';
 import 'package:mhg/features/myorders/view/widgets/tab_bar_card.dart';
 import 'package:mhg/widgets/custom_app_bar.dart';
+import 'package:mhg/widgets/retry_button.dart';
 import '../widgets/my_orders_list_widget.dart';
 
 class MyOrdersPage extends StatefulWidget {
@@ -70,17 +73,31 @@ class _MyOrdersPageState extends State<MyOrdersPage>
                   ),
                 ],
               ),
-              Expanded(
-                child: TabBarView(
-                  physics: const NeverScrollableScrollPhysics(),
-                  controller: _tabController,
-                  children: const [
-                    MyOrdersListWidget(),
-                    MyOrdersListWidget(),
-                    MyOrdersListWidget(),
-                  ],
-                ),
-              ),
+       GetX<MyOrdersController>(builder: (controller) {
+      if (controller.isLoading.isTrue) {
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      } else if (controller.isError.isTrue) {
+        return RetryButton(
+          onTap: () {
+            controller.getMyOrders();
+          },
+        );
+      }
+      return  Expanded(
+        child: TabBarView(
+          physics: const NeverScrollableScrollPhysics(),
+          controller: _tabController,
+          children:  [
+            MyOrdersListWidget(model: controller.orders,),
+            MyOrdersListWidget(model: controller.returns,),
+            MyOrdersListWidget(model: controller.cancelled,),
+          ],
+        ),
+      );
+    })
+
             ],
           )),
     );
--- lib/features/myorders/view/widgets/my_orders_list_widget.dart
+++ lib/features/myorders/view/widgets/my_orders_list_widget.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:mhg/features/myorders/models/order_model.dart';
 import '../../../../constants/app_colors.dart';
 import 'my_order_card.dart';
 
 class MyOrdersListWidget extends StatelessWidget {
-  const MyOrdersListWidget({super.key});
+  final List <MyOrder> model;
+  const MyOrdersListWidget({super.key, required this.model});
 
   @override
   Widget build(BuildContext context) {
     return ListView.separated(
       physics: const BouncingScrollPhysics(),
-      itemCount: 4,
+      itemCount: model.length,
       itemBuilder: (context, index) {
-        return MyOrderCard();
+        return MyOrderCard(model: model[index],);
       },
       separatorBuilder: (context, index) {
         return Divider(
