e8ac6c5b5b3cd349dd76829d5ea697079cf36dc0
AliMzek <ali_mzek@outlook.com>
2023-06-25 16:17:23 +0300

    details

--- lib/features/myorders/view/widgets/my_order_details_global_info.dart
+++ lib/features/myorders/view/widgets/my_order_details_global_info.dart
@@ -14,12 +14,12 @@ class MyOrderDetailsGlobalInfo extends StatelessWidget {
   Widget build(BuildContext context) {
     log(model.grandTotal);
     return Padding(
-      padding: const EdgeInsets.all(20.0),
+      padding: const EdgeInsets.symmetric(vertical: 20.0,horizontal: 10),
       child: Column(
 
         children: [
           Row(
-            crossAxisAlignment: CrossAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Expanded(
@@ -27,7 +27,7 @@ class MyOrderDetailsGlobalInfo extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 1),
                   child: Center(
                     child: Text(
-                      'TAX Amount\n${model.tax}',
+                      'TAX \n\n${model.tax}',
 
 
                       style: Theme.of(context).textTheme.displaySmall?.copyWith(
@@ -60,7 +60,7 @@ class MyOrderDetailsGlobalInfo extends StatelessWidget {
                   padding: const EdgeInsets.symmetric(horizontal: 1),
                   child: Center(
                     child: Text(
-                      'Sub Total\n${model.subTotal}',
+                      'Sub Total\n\n${model.subTotal}',
 
                       style: Theme.of(context).textTheme.displaySmall?.copyWith(
                         fontSize: 14,
