cf9fa87daf4dd6e76ce1e70aa7599c9782d9f8fa
mhamad <mhamad.ibrahem532@gmail.com>
2023-07-02 20:37:11 +0300

    edit wishlist card and cart card and checkout card

--- lib/features/my_wish_list/view/widget/my_wish_list_body.dart
+++ lib/features/my_wish_list/view/widget/my_wish_list_body.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mhg/constants/app_dimensions.dart';
 import 'package:mhg/features/home/controller/home_controller.dart';
 import 'package:mhg/features/my_wish_list/controller/wish_list_controller.dart';
 import 'package:mhg/features/my_wish_list/model/wish_list_model.dart';
@@ -46,7 +45,8 @@ class _MyWishBodyState extends State<MyWishBody> {
           child: Row(
             children: [
               Container(
-                height: AppDimensions.productHeight(context) / 1.3,
+                // height: AppDimensions.productHeight(context) / 1.2,
+                // height: 170,
                 width: 120,
                 decoration: const BoxDecoration(
                   borderRadius:
@@ -55,7 +55,10 @@ class _MyWishBodyState extends State<MyWishBody> {
                 child: ClipRRect(
                     borderRadius: const BorderRadius.horizontal(
                         left: Radius.circular(12)),
-                    child: NetImage(image: widget.model.options.imageLink)),
+                    child: NetImage(
+                      image: widget.model.options.imageLink,
+                      fit: BoxFit.fitHeight,
+                    )),
               ),
               Expanded(
                   child: Stack(
@@ -65,12 +68,17 @@ class _MyWishBodyState extends State<MyWishBody> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const SizedBox(height: 35),
-                        TopBodyHeader(
-                            title:  isAR() 
-                                ? widget.model.options.arName
-                                : widget.model.options.enName),
-                        const SizedBox(height: 10),
+                        const SizedBox(height: 32),
+                        SizedBox(
+                          height: 25,
+                          child: FittedBox(
+                            child: TopBodyHeader(
+                                title: isAR()
+                                    ? widget.model.options.arName
+                                    : widget.model.options.enName),
+                          ),
+                        ),
+                        // const SizedBox(height: 10),
                         BodyMiddleText(
                           brand: widget.model.options.brand,
                           price: widget.model.price.toString(),
@@ -81,7 +89,7 @@ class _MyWishBodyState extends State<MyWishBody> {
                     ),
                   ),
                   Align(
-                    alignment:   Alignment.topRight,
+                    alignment: Alignment.topRight,
                     child: widget.model.isDeleteItem
                         ? Container(
                             height: 25,
