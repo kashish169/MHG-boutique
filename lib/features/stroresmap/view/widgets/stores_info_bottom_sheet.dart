37f45778570840ac7279a82e7bfd7f329d7ac970
mahmoud <mahmoudhabib.eng@gmail.com>
2023-08-14 11:00:48 +0300

    fix ui

--- lib/features/stroresmap/view/widgets/stores_info_bottom_sheet.dart
+++ lib/features/stroresmap/view/widgets/stores_info_bottom_sheet.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mhg/constants/app_colors.dart';
+import 'package:mhg/core/helper/app_helper.dart';
 
-showStoresBottomSheet(List<String>? places) => Get.bottomSheet(
+showStoresBottomSheet(List<String>? places, name, tel , {second_tel}) => Get.bottomSheet(
       Container(
         height: Get.height * 0.35,
-        color: AppColors.dGreen.withOpacity(0.8),
+        color: Colors.white,
         child: SingleChildScrollView(
           child: Stack(
             children: [
@@ -14,41 +15,140 @@ showStoresBottomSheet(List<String>? places) => Get.bottomSheet(
                 top: 15,
                 child: SizedBox(
                   height: Get.height * 0.02,
-                  child: const Text(
+                  child: Text(
                     'STORES',
                     style: TextStyle(
                       fontSize: 18,
-                      color: Colors.white,
+                      color: AppColors.dGreen,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
                 ),
               ),
               Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: List.generate(
-                  places?.length ?? 0,
-                  (index) => Padding(
-                    padding: index == 0
-                        ? EdgeInsets.only(top: Get.height * 0.1 , bottom: 8, )
-                        : EdgeInsets.all(8),
-                    child: Center(
-                      child: SizedBox(
-                        height: Get.height * 0.02,
-                        child: Text(
-                          places?[index] ?? 'NO STORES AVAILABLE',
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold,
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: List.generate(
+                      places?.length ?? 0,
+                      (index) => Padding(
+                        padding: index == 0
+                            ? EdgeInsets.only(
+                                top: Get.height * 0.065,
+                                bottom: 8,
+                              )
+                            : EdgeInsets.all(8),
+                        child: Center(
+                          child: SizedBox(
+                            height: Get.height * 0.02,
+                            child: Text(
+                              places?[index] ?? 'NO STORES AVAILABLE',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontSize: 16,
+                                color: AppColors.dGreen,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
                           ),
                         ),
                       ),
                     ),
                   ),
-                ),
+                  SizedBox(
+                    height: Get.height * 0.03,
+                  ),
+                  Text(
+                    'CALL US',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: AppColors.dGreen,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(
+                    height: Get.height * 0.03,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      AppHelper.launchURL(tel, 'tel');
+                    },
+                    child: Container(
+                      margin: EdgeInsets.only(
+                        left: 8,
+                        right: Get.width * 0.02,
+                      ),
+                      child: Table(
+                        border: null,
+                        children: [
+                          TableRow(
+                            children: [
+                              Text(
+                                name,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.dGreen,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                tel,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.dGreen,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 50,
+                                child: Padding(
+                                  padding: EdgeInsets.only(bottom: 25),
+                                  child: Icon(
+                                    Icons.call,
+                                    color: AppColors.dGreen,
+                                  ),
+                                ),
+                              ),
+                            
+                            ],
+                          ) , 
+                           second_tel != null ? TableRow(
+                            children: [
+                              Text(
+                                name,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.dGreen,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                second_tel,
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: AppColors.dGreen,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Icon(
+                                Icons.call,
+                                color: AppColors.dGreen,
+                              ),
+                            
+                            ],
+                          ) :const TableRow(children: [SizedBox() , SizedBox() , SizedBox()])
+                        ],
+                      ) 
+                      ,
+                    ),
+                  )
+                ],
               ),
             ],
           ),
